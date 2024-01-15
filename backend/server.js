2fcd8a12f6e579cabe329bb5edb941cef38a20eb
Archit <109897872+im-Archit@users.noreply.github.com>
2024-01-15 14:34:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,15 +29,16 @@ app.use("/api/message", messageRoutes);
 
 // Deployment 
 
-const __dirname1 = path.resolve();
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+const __dirname1 = __dirname;
 
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
-  })
-}else{
-  app.get("/",(req,res)=>{
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
     res.send("API is running Successfully");
   });
 }
